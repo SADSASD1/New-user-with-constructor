be73860d0a146de1e8e02234716c429cf3e9805e
SADSASD1 <aleqsanyan1996@.ru>
2018-11-18 12:53:47 +0400

    User calling with constructor

---
+++ User.java
@@ -0,0 +1,65 @@
+public class User {
+    private String firstname = new String();
+    private String lastname = new String();
+    private String email = new String();
+    private String password = new String();
+
+    public User() {
+
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+    public User(String firstname, String lastname, String email, String password) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+
+    }
+    public void saying(){
+        System.out.println(firstname);
+        System.out.println(lastname);
+        System.out.println(email);
+        System.out.println(password);
+    }
+}
+
+
+
+
+
+
+
