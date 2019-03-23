a8ef917008da047487b9d233f47336951132e1d1
SADSASD1 <ash.aleqsanyan96@gmail.com>
2019-03-23 18:58:18 +0400

    My List

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
-
+import java.lang.String;
 import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         User us = new User("Ashot", "Aleqsanyan", "aleqsanyan1996@mail.ru", "1234qwer");
@@ -7,16 +9,19 @@ public class Main {
         User karen  = new User ("Karen", "Karapetyan", "Karen@gmail.com", "asdf1234");
         User hayk  = new User ("Hayk", "Ohanyan", "Hayk125@gamil.com", "44rr44");
 
-        ArrayList<User>User=new ArrayList<User>(3);
-         User.add(us);
-         User.add(vahan);
-         User.add(karen);
-         User.add(hayk);
+        List myList = new ArrayList();
+        myList.add(us);
+        myList.add(vahan);
+        myList.add(karen);
+        myList.add(hayk);
 
-        System.out.println(User);
+        for(Object next:myList){
+            System.out.println(next);
+        }
+        }
 
     }
-}
+
 
 
 
